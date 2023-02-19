1c61a58f21fa163bdda11ff175d712b02d54845a
Ashtonmey <ashcalebmeyer@gmail.com>
2023-02-19 11:21:53 +0200

    Added event listeners, and variable sstorage function calls to operator events

--- calculator.js
+++ calculator.js
@@ -1,10 +1,12 @@
-// Display variable
+// Declare variables
 
-let displayNumber = "";
+let displayNumber = ""; // intitial dsiplay number variable
+let operatorChoice = null; // Initial declare of operator choice
+let operatorCount = 0; // counts the number of times and operator has been clicked
+let numbers = [];
 
-// add eventlisteners to each button
 
-let number = document.querySelector(`#display-number`);
+// add eventlisteners to each number button
 
 const buttons = document.querySelectorAll(`.number`);
 
@@ -16,12 +18,73 @@ buttons.forEach((button) => {
 
 });
 
-// update display on button click fuction
+// add event listener to clear button
+
+const clearButton = document.querySelector("#c");
+clearButton.addEventListener('click', () => {
+    clear();
+});
+
+// add event listeners to operator buttons
+
+const opButtons = document.querySelectorAll (`.operator`);
+
+opButtons.forEach((opButton)  => {
+
+opButton.addEventListener('click', (e) => {    
+    saveNumber(e);        
+    })
+
+opButton.addEventListener('click', (e) => {
+    saveOperator(e);
+})
+    
+});
+
+
+// clear function 
+
+function clear () {
+    displayNumber = "";
+    operatorChoice = null;
+    operatorCount = 0;
+    numbers = [];
+
+    const display = document.querySelector(`#display-number`);
+    display.textContent = "0";
+
+}
+
+// update display on numerical button click function
 
 function updateDisplay (e) {
     const display = document.querySelector(`#display-number`);
     displayNumber = displayNumber.concat(e.target.id);
-    display.textContent = displayNumber;   
+    display.textContent = displayNumber;  
+}
+
+// save operator function
+
+function saveOperator (e){
+    operatorChoice = e.target.id;
+    console.log(operatorChoice);
+}
+
+// save number function
+
+function saveNumber (e){
+    numbers[operatorCount] = displayNumber;    
+    operatorCount++;
+    clearDisplay();
+    console.log(numbers);
+}
+
+// function to clear display 
+
+function clearDisplay () {
+    const display = document.querySelector(`#display-number`);
+    display.textContent = "0";
+    displayNumber = "";
 }
 
 // operator function
